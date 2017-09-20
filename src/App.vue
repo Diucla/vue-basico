f652bf0048bea8a9f058bef0dc8d25f433d3cabd
Cláudio Sumburane <claudiosingular@gmail.com>
2017-09-20 12:38:16 +0200

    Diretives

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-	<div class="phone-viewport">
+	<header class="phone-viewport">
         <md-toolbar>
-            <md-button class="md-icon button" v-on:click="toggleLeftSidenav">
+            <md-button class="md-icon button" @click.native="toggleLeftSidenav">
                 <md-icon>menu</md-icon>
             </md-button>
+
             <h1 class="md-title">{{ title }}</h1>
         </md-toolbar>
-    </div>
+
+        <md-sidenav class="md-left" rel="leftSidenav">
+            <md-toolbar class="md-medium">
+                <div class="md-toolbar-container">
+                    <h2 class="md-title">Sidenav content</h2>
+                </div>
+
+                <p>
+                    Lorem Ipsum dolor
+                </p>
+            </md-toolbar>
+        </md-sidenav>
+    </header>
 </template>
 
 <script>
@@ -16,9 +29,12 @@
       return {
         title: 'Ecommerce by Sumbus'
       }
+    },
+
+    methods: {
+      toggleLeftSidenav () {}
     }
   }
-
 </script>
 
 <style>
