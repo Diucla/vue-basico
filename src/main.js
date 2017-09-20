8cdc34daa77cfd4dab88aa375ffa6f7b08d0c993
Cláudio Sumburane <claudiosingular@gmail.com>
2017-09-20 05:46:07 +0200

    Instalacao do VueMAterial
    Adicao de algumas imagens too, alguns testes de Hello World.... bla bla blaaa

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
+
+Vue.material.registerTheme('default', {
+  primary: 'blue',
+  accent: 'red',
+  warn: 'red',
+  background: 'white'
+})
 
 /* eslint-disable no-new */
 new Vue({
