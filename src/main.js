1e33ca562f4e0e299b34b7d6320f94ba574d06ee
Cláudio Sumburane <claudiosingular@gmail.com>
2017-09-20 12:44:52 +0200

    Adicao de Router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
@@ -8,6 +9,7 @@ import 'vue-material/dist/vue-material.css'
 Vue.config.productionTip = false
 
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
 
 Vue.material.registerTheme('default', {
   primary: 'blue',
